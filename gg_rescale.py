c72579a915f99591fde8900535ffa1d293d103df
Hannah Wakeford <hrwakeford@users.noreply.github.com>
2019-05-06 09:45:50 -0400

    Modification on gg_rescale.py
    Added in the ability to rescale the model with an input mean molecular weight scaling for the atmosphere. Changed the variable names from two letter variables to descriptive words. Added in a second run in the main: to show how the mean molecular weight scaling compares to the default of a H/He-dominated atmosphere.

--- gg_rescale.py
+++ gg_rescale.py
@@ -1,9 +1,11 @@
 # ggg_rescale.py
 import numpy as np 
 import os
+import matplotlib as mpl
+# mpl.use("TkAgg") # uncomment if working woth MacXCode in Mac 10.14 or higher
 import matplotlib.pyplot as plt
 
-def gg_rescale(model_dir, model_file, rs, rp, gp, tp):
+def gg_rescale(model_dir, model_file, radius_star, radius_planet, gravity_planet, temperature_planet, mean_molecular_weight=2.3):
 
 	"""
 	gg_rescale
@@ -16,20 +18,22 @@ def gg_rescale(model_dir, model_file, rs, rp, gp, tp):
 		e.g. '/Users/name/Documents/Generic_Goyal_Grid/Rainout_condensation/'
 	file = string of the model name you want to run the input file should be selected such that it is closest to the gravity and temperature for the planet
 		e.g. trans-iso-generic_1500_05_+0.0_0.56_0001_0.00_model.txt'
-	rs = radius of the star in solar radii
-	rp = radius of the planet in jupiter radii
-	gp = gravity of the planet in cgs units
-	tp = temperature of the planet in K
-
+	radius_star = radius of the star in solar radii
+	radius_planet = radius of the planet in jupiter radii
+	gravity_planet = gravity of the planet in cgs units
+	temperature_planet = temperature of the planet in K
+	mean_molecular_weight = scale factor for the atmospheric mean molecular weight. This will be multiplied by the mass of a proton and used to calculate the planetary scale height. The default is set to 2.3 for H/He-dominated atmosphere.
+	
 	OUTPUTS:
-	wav = wavlength array for the model (microns)
-	depth = (Rp/R*)^2 model
+	model_wavelength = wavlength array for the model (microns)
+	transit_depth = (Rp/R*)^2 model
 	"""
 
 
 	# SET UP THE CONSTANTS
 	kb = 1.380658E-16 # gm*cm^2/s^2 * Kelvin
-	mu = 1.6726E-24 * 2.3 #g  cgs  Hydrogen + Helium Atmosphere
+	mu_model = 1.6726E-24 * 2.3 #g  cgs  Hydrogen + Helium Atmosphere
+	mu_planet = 1.6726E-24 * mean_molecular_weight #g  cgs rescaled atmospheric mean molecular weight
 	tau = 0.56 # optical depth
 	rsun = 69580000000 # cm
 	rjup = 6991100000 # cm
@@ -41,39 +45,38 @@ def gg_rescale(model_dir, model_file, rs, rp, gp, tp):
 
 	model_wav, model_rp = np.loadtxt(model_filename, unpack=True)
 
-	model_rprs = np.sqrt(model_rp) * (rp/rs)
+	model_rprs = np.sqrt(model_rp) * (radius_planet/radius_star)
 
-	rstar = rs * rsun
-	rpl = rp * rjup
+	rstar = radius_star * rsun
+	rpl = radius_planet * rjup
 	rp2 = rpl
 
 	# Extract the starting temperature and gravity from the input filename
-	t1 = float(model_file.split('_')[1])
-	g1 = float(model_file.split('_')[2])
-	g1 = g1*1e2 #rescaled to cm
+	temperature_model = float(model_file.split('_')[1]) # Pull the temperature of the model from the filename
+	gravity_model = float(model_file.split('_')[2]) # Pull the gravity of the model from the filename
+	gravity_model = gravity_model*1e2 # gravity of the selected model rescaled to cm
 
 	# Calculate the transmission spectrum based on the file parameters
 	#	 to get the baseline radius
-	h1 = (kb * t1) / (mu * g1)
+	h1 = (kb * temperature_model) / (mu_model * gravity_model) # Calculate the scale height of the model atmosphere
 	rp1 = np.sqrt(model_rp) * rsun #cm
 	z1 = rp1 - (np.sqrt(model_rp[2000])*rsun) #cm
-	epsig1 = tau * np.sqrt((kb * t1 * mu * g1) / (2. * np.pi * rp1)) * np.exp(z1 / h1)
+	epsig1 = tau * np.sqrt((kb * temperature_model * mu_model * gravity_model) / (2. * np.pi * rp1)) * np.exp(z1 / h1)
 
-	# Rescale based on the input parameters
-
-	h2 = (kb * tp) / (mu * gp)
-	z2 = h2 * np.log(epsig1 / tau * np.sqrt((2. * np.pi * rp2)/(kb * tp * mu * gp)))
+	# Rescale the model atmosphere based on the input parameters
+	h2 = (kb * temperature_planet) / (mu_planet * gravity_planet)
+	z2 = h2 * np.log(epsig1 / tau * np.sqrt((2. * np.pi * rp2)/(kb * temperature_planet * mu_planet * gravity_planet)))
 	r2 = z2 + rp2
 
-# Resort the data so it goes from lowest wavelength to highest
+	# Resort the data so it goes from lowest wavelength to highest
 	srt = np.argsort(model_wav)
-	wav = model_wav[srt]
-	r2 = r2[srt]
+	model_wavelength = model_wav[srt]
+	radius_sort = r2[srt]
 
-	depth = (r2 / rstar)**2
+	transit_depth = (radius_sort / rstar)**2 # Output is in (Rp/Rs)^2
 
-# These are the OUTPUTS
-	return wav, depth
+	# These are the OUTPUTS
+	return model_wavelength, transit_depth
 
 
 
@@ -92,24 +95,39 @@ if __name__ == '__main__':
 	model_dir = './Data'
 	model_file = 'trans-iso-generic_0400_05_+0.0_0.35_0001_0.06_model.txt'
 	# model_file = 'trans-iso-generic_1500_05_+0.0_0.56_0001_0.00_model.txt'
-	rs = 1.100 # Rstar
-	rp = 1.200 # RJ
-	gp = 610 # cgs
-	tp = 450 # K
-
+	
+	# Set up your input values for the planet system
+	radius_star = 1.100 # Rstar
+	radius_planet = 1.200 # RJ
+	gravity_planet = 610 # cgs
+	temperature_planet = 450 # K
+	# mean_molecular_weight = 2.3 
+	# If mean_molecular_weight is not set it will use 2.3 as a defult (H+He atmosphere)
+	
 	# result = gg_rescale(model_dir, model_file, rs, rp, gp, tp)
 	# This will give you a tuple with wavelength and transit depth.
 	# Or do the following to put them into your own arrays
-	wav, rprs = gg_rescale(model_dir, model_file, rs, rp, gp, tp)
+	wav_default_mmw, rprs_default_mmw = gg_rescale(model_dir, model_file, radius_star, radius_planet, gravity_planet, temperature_planet)
 
 
-	plt.plot(wav, rprs)
+
+	# Run 2 with your own input mean molecular weight
+	radius_star = 1.100 # Rstar
+	radius_planet = 1.200 # RJ
+	gravity_planet = 610 # cgs
+	temperature_planet = 450 # K
+	mean_molecular_weight = 5 # Rescale the atmosphere to your own mean molecular weight.
+
+	wav_set_mmw, rprs_set_mmw = gg_rescale(model_dir, model_file, radius_star, radius_planet, gravity_planet, temperature_planet, mean_molecular_weight)
+
+	
+	# Plot the default mean molecular weight atmosphere against the scaled one
+	plt.plot(wav_default_mmw, rprs_default_mmw)
+	plt.plot(wav_set_mmw, rprs_set_mmw, color='red')
 	plt.xlim(0.3,15)
 	plt.xscale('log')
 	plt.tight_layout()
 	plt.show()
-
-
 
 
 
